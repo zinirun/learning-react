977f1034cfe7c9e39b86ca3645391b6c637f9b57
zinirun <heojj97@naver.com>
2020-08-13 15:35:37 +0900

    Create debounce.js

---
+++ src/02/debounce.js
@@ -0,0 +1,14 @@
+function debounce(func, delay) {
+  let inDebounce;
+  return function (...args) {
+    if (inDebounce) {
+      clearTimeout(inDebounce);
+    }
+    inDebounce = setTimeout(() => func(...args), delay);
+  };
+}
+
+const run = debounce((val) => console.log(val), 100);
+run("a");
+run("b");
+run(2);
