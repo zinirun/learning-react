bdd299c8b2bf6eda8924dceaf7cb582ecddb0465
zinirun <heojj97@naver.com>
2020-10-27 21:14:18 +0900

    Add Immer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useReducer, useRef, useMemo, createContext } from '
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import useInputsReducer from './useInputs';
+import produce from 'immer'; // 불변성 유지
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -34,22 +35,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user,
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       throw new Error('Unhandled action');
   }
